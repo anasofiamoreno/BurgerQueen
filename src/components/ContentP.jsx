ec100679892513d60cea13013655e88df9397937
anasofiamoreno <84933610+anasofiamoreno@users.noreply.github.com>
2021-10-17 23:54:22 -0500

    Test of repair fail on child

--- src/components/ContentP.jsx
+++ src/components/ContentP.jsx
@@ -24,9 +24,9 @@ console.log(Object.entries(state.costumers).length)
     <div id="idContentP" key={uuidv4()} className="cContentP">
       <button onClick={fnChange}>Simulate LogIn</button>
       <LogIn />
-      {Object.entries(state.costumers).length !==0 && <MenuBQ onClick={fnChange} state={state} fnData = {fnData}/> }
-      {Object.entries(state.costumers).length !==0 && <ResumeOrder onClick={fnChange} state={state}/> }
-      {Object.entries(state.costumers).length === 0 && <img onClick={fnChange} className='cImgBack' src={Logo} alt='Logo BurgerQueen'></img> }
+      {Object.entries(state.costumers).length !==0 && <MenuBQ key={uuidv4()}  state={state} fnData = {fnData}/> }
+      {Object.entries(state.costumers).length !==0 && <ResumeOrder key={uuidv4()}  state={state}/> }
+      {Object.entries(state.costumers).length === 0 && <img key={uuidv4()} className='cImgBack' src={Logo} alt='Logo BurgerQueen'></img> }
     </div>
 
   )
